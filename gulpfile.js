0cbababcf16f7e65cef0ca4fd7f7d80c21084023
Konstantin Popov <konst.hardy@gmail.com>
2020-01-08 23:32:55 +0300

    Add Gulp files

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+
+
+const cssFiles = [
+	'./src/css/swiper.min.css',
+	'./src/css/styles.css',
+]
+
+const jsFiles = [
+	'./src/js/swiper.min.js',
+	'./src/js/jquery.inputmask.min.js',
+	'./src/js/jquery.validate.min.js',
+	'./src/js/script.js',
+]
+
+
+function styles() {
+	// return gulp.src('./src/css/**/*.css')
+	return gulp.src(cssFiles)
+	.pipe(concat('all.css'))	
+	// .pipe(autoprefixer({
+	// 	cascade: false,
+	// }))
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(gulp.dest('./build/css'))
+}
+
+function scripts() {
+	// return gulp.src('./src/js/**/*.js')	
+	return gulp.src(jsFiles)
+	.pipe(concat('all.js'))	
+	.pipe(uglify({
+		toplevel:true,
+	}))
+	.pipe(gulp.dest('./build/js'))
+}
+
+
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
